153e0194832a58db7d25cb02bc77fb6a306731f5
Albert <mosgfrrfgsom@gmail.com>
2023-04-03 00:41:35 +0300

    Created and implemented the class BubbleSort

---
+++ Adapter/Implementations/BubbleSort.cs
@@ -0,0 +1,34 @@
+﻿using Adapter.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Adapter.Implementations
+{
+	internal class BubbleSort : IBubbleSortForInt
+	{
+		public void Swap(ref int firstArgument, ref int secondArgument)
+		{
+			var temp = firstArgument;
+			firstArgument = secondArgument;
+			secondArgument = temp
+		}
+
+		public void BubbleSortt(int[] array)
+		{
+			for (int i = 0; i < array.Length; i++)
+			{
+				for (int j = 0; j < array.Length - 1 - i; j++)
+				{
+					if (array[j] > array[j + 1])
+					{
+						Swap(ref array[j], ref array[j + 1]);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Adapter/Interfaces/IBubbleSort.cs
@@ -0,0 +1,9 @@
+﻿namespace Adapter.Interfaces
+{
+	internal interface IBubbleSortForInt<T>
+	{
+		public void BubbleSortt(T[] array);
+
+		public void Swap(ref T firstArgument, ref T secondArgument);
+	}
+}
